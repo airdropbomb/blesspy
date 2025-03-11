84de2fec7602d4a31b95102cb7b72f261f4d2575
airdropbomb <saginaw135@gmail.com>
2025-03-11 12:19:52 +0700

    Update logger.py

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,20 +1,26 @@
 from datetime import datetime
 
+from colorama import Fore, Style, init
+
+init(autoreset=True)
+
 
 class Logger:
     @staticmethod
-    def log_message(current_num=None, total=None, message="", message_type="info"):
+    def log_message(current_num=None, total=None, message="", message_type="info", end="\n"):
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         account_status = f"[{current_num}/{total}] " if current_num and total else ""
 
         colors = {
-            "info": "\033[97m",
-            "success": "\033[92m",
-            "error": "\033[91m",
-            "warning": "\033[93m",
-            "process": "\033[96m",
-            "debug": "\033[94m",
+            "info": Fore.WHITE,
+            "success": Fore.GREEN,
+            "error": Fore.RED,
+            "warning": Fore.YELLOW,
+            "process": Fore.CYAN,
+            "debug": Fore.BLUE,
         }
 
-        color = colors.get(message_type, "\033[97m")
-        print(f"\033[97m[{timestamp}] {account_status}{color}{message}\033[0m")
+        color = colors.get(message_type, Fore.WHITE)
+        print(
+            f"{Fore.WHITE}[{timestamp}] {account_status}{color}{message}{Style.RESET_ALL}", end=end
+        )
